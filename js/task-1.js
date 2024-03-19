54241122eda927e0ee695fe930a47d8417fecd5e
AndriyTkh <151773877+AndriyTkh@users.noreply.github.com>
2024-03-19 21:14:55 +0200

    finished

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,21 @@
-function getExtremeScores(scores) {
-  const obj = {
-    best: null,
-    worst: null,
-  };
+"use strict";
 
-  obj.best = Math.max(...scores);
-  obj.worst = Math.min(...scores);
+function isEnoughCapacity(products, containerSize) {
+  let weight = 0;
 
-  return obj;
+  for (const key in products) {
+    weight += products[key];
+  }
+
+  if (weight <= containerSize) return true;
+  else return false;
 }
+
+console.log("---------------- task (1) ------------------");
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
